0f5e7e114b155126ac2c44fc2bac5c2e6813a6de
nayon117 <hasibulhasannayon260@gmail.com>
2023-02-12 09:31:26 +0600

    update script

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://nse-cpkd.onrender.com', {
+    const response = await fetch('https://open-ai-codex-i3p9.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
